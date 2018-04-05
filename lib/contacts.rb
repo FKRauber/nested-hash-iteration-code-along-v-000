d80db0810cd9a7d5de8f300206c0ffd642f53fec
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-05 22:07:10 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,8 +17,8 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
-      if value == "strawberry"
-        delete value
+      if attribute == :favorite_ice_cream_flavors
+        value.delete_if { |ice_cream| ice_cream == "strawberry" }
       end
     end
   end
